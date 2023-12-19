2c63bf25880f0b82f2d19494f04bc37b13848005
Abscract girl <abstract.girl@gmail.com>
2023-12-19 09:33:09 +0000

    fix tests to take by id

--- get_cache.lua
+++ get_cache.lua
@@ -1,11 +1,19 @@
-local frandom = io.open("/dev/urandom", "rb")
-local d = frandom:read(4)
-math.randomseed(d:byte(1) + (d:byte(2) * 256) + (d:byte(3) * 65536) + (d:byte(4) * 4294967296))
+local json = require 'cjson'
 
-number =  math.random(1,100000)
+local ids = {}
+
+local file = io.open('ids.json', 'r')
+if file then
+    ids = json.decode(file:read('*all'))
+    file:close()
+end
+
+math.randomseed(os.time())
+
 request = function()
     headers = {}
-    headers["Content-Type"] = "application/json"
+    headers['Content-Type'] = 'application/json'
     body = ''
-    return wrk.format("GET", "/user?id=".. tostring(number), headers, body)
+    local id = ids[math.random(#ids)]
+    return wrk.format('GET', '/user?id='.. id, headers, body)
 end
--- get_no_cache.lua
+++ get_no_cache.lua
@@ -1,11 +1,23 @@
-local frandom = io.open("/dev/urandom", "rb")
-local d = frandom:read(4)
-math.randomseed(d:byte(1) + (d:byte(2) * 256) + (d:byte(3) * 65536) + (d:byte(4) * 4294967296))
+-- local frandom = io.open("/dev/urandom", "rb")
+-- local d = frandom:read(4)
+-- math.randomseed(d:byte(1) + (d:byte(2) * 256) + (d:byte(3) * 65536) + (d:byte(4) * 4294967296))
 
-number =  math.random(1,100000)
+local json = require 'cjson'
+
+local ids = {}
+
+local file = io.open('ids.json', 'r')
+if file then
+    ids = json.decode(file:read('*all'))
+    file:close()
+end
+
+math.randomseed(os.time())
+
 request = function()
     headers = {}
-    headers["Content-Type"] = "application/json"
+    headers['Content-Type'] = 'application/json'
     body = ''
-    return wrk.format("GET", "/user?no_cache&id=".. tostring(number), headers, body)
+    local id = ids[math.random(#ids)]
+    return wrk.format('GET', '/user?no_cache&id='.. id, headers, body)
 end
